2ae1dbf8e2941d35f76538a5af06fcd5228a7b97
radelitsca <radelitsca@gmail.com>
2021-12-09 22:42:59 +0200

    hw, clw

---
+++ lesson11/homework.js
@@ -0,0 +1,24 @@
+// -створити форму з інпутами для name та age.
+//     При відправці форми записати об'єкт в localstorage
+// let form = document.forms.input;
+// form.onsubmit = e => {
+//     e.preventDefault();
+//     let uname = this.username.value;
+//     let age = this.age.value;
+//     let user = {name: uname, age: age};
+//     localStorage.setItem('user', JSON.stringify(user));
+// }
+
+// -створити форму з інпутами для model,type та volume автівки.
+//     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+// let form = document.forms.input;
+// form.onsubmit = e => {
+//     e.preventDefault();
+//     let model = this.carmodel.value;
+//     let type = this.type.value;
+//     let volume = this.volume.value;
+//     let car = [{model: model}, {type: type}, {volume: volume}];
+//     localStorage.setItem('car', JSON.stringify(car));
+// }
+
+
